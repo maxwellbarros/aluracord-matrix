88cdbd820549380592f0755dcfcfc48337e1e560
Maxwell Barros <maxwellbarros@yahoo.com.br>
2022-01-25 14:09:11 -0300

    State, novas páginas e navegação SPA vs a Tradicional

--- pages/index.js
+++ pages/index.js
@@ -1,36 +1,13 @@
 
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
 import { loadGetInitialProps } from "next/dist/shared/lib/utils";
+import React from 'react';
 import appConfig from './config.json';
+import {ReactComponent as StarWarsImg} from './img/star-wars-the-lars-homestead-at-tatooine.jpg';
+import { useRouter } from "next/router";
 
-function GlobalStyle() {
-    return (
-      <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-  }
+
+
 function Titulo(props) {
     const Tag = props.tag;
   return (
@@ -61,11 +38,12 @@ function Titulo(props) {
 //export default HomePage;
 
 export default function PaginaInicial() {
-    const username = 'maxwellbarros';
+    //const username = 'maxwellbarros';
+    const [username, setUsername] = React.useState('maxwellbarros');
+    const roteamento = useRouter();    
   
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
@@ -92,6 +70,12 @@ export default function PaginaInicial() {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={function(infosDoEvento){
+                  infosDoEvento.preventDefault();
+                  console.log('Alguém submeteu o Form');
+                  roteamento.push('/chat');
+                  //window.location.href = '/chat';
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -101,8 +85,30 @@ export default function PaginaInicial() {
               <Text variant="body3" styleSheet={{ marginBottom: '32px', color: appConfig.theme.colors.neutrals[300] }}>
                 {appConfig.name}
               </Text>
-  
+              {/*<input
+                type="text"
+                value={username}
+                onChange={function handler(event){
+                  console.log('Usuario digitou',event.target.value);
+                  //onde tá o valor
+                  const valor = event.target.value;
+                  //trocar o valor da variável
+                  //através do react e avisa quem precisa
+                  setUsername(valor);
+
+                }}
+              />    */    }   
               <TextField
+                value={username}
+                onChange={function handler(event){
+                  console.log('Usuario digitou',event.target.value);
+                  //onde tá o valor
+                  const valor = event.target.value;
+                  //trocar o valor da variável
+                  //através do react e avisa quem precisa
+                  setUsername(valor);
+
+                }}
                 fullWidth
                 textFieldColors={{
                   neutral: {
